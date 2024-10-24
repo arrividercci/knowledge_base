81f71a03df4b3547ef8ddf73e7107cecf0961d52
Vlad Kuchinskyi <109318952+arrividercci@users.noreply.github.com>
2024-10-24 10:00:12 +0300

    Update program.pl

--- program.pl
+++ program.pl
@@ -1,35 +1,45 @@
 :- discontiguous is_a/2.
 :- discontiguous part_of/2.
-
 :- table is_a/2.
 :- table part_of/2.
 
-is_a(fashion, category).
+is_a(X, Z) :- 
+    is_a(X, Y), 
+    is_a(Y, Z).
 
+is_a(fashion, category).
 is_a(clothing, fashion).
 is_a(accessories, fashion).
 is_a(headwear, fashion).
 is_a(footwear, fashion).
-
 is_a(jacket, clothing).
 is_a(pants, clothing).
 is_a(shirt, clothing).
 is_a(dress, clothing).
-
 is_a(watch, accessories).
 is_a(chain, accessories).
 is_a(bracelet, accessories).
 is_a(sunglasses, accessories).
-
 is_a(cap, headwear).
 is_a(winter_hat, headwear).
 is_a(baseball_cap, headwear).
 is_a(beanie, headwear).
-
 is_a(sneakers, footwear).
 is_a(shoes, footwear).
 is_a(sandals, footwear).
 is_a(slippers, footwear).
+
+part_of(X, Z) :- 
+    part_of(X, Y), 
+    part_of(Y, Z).
+
+part_of(X, Z) :- 
+    part_of(X, Y), 
+    is_a(Y, Z).
+
+part_of(X, Z) :- 
+    is_a(X, Y), 
+    part_of(Y, Z).
 
 part_of(zipper, jacket).
 part_of(pocket, jacket).
@@ -52,20 +62,3 @@ part_of(heel, shoes).
 part_of(toe_box, shoes).
 part_of(sole, sandals).
 part_of(insole, slippers).
-
-is_a(X, Z) :- 
-    is_a(X, Y), 
-    is_a(Y, Z).
-
-part_of(X, Z) :- 
-    part_of(X, Y), 
-    part_of(Y, Z).
-
-part_of(X, Z) :- 
-    part_of(X, Y), 
-    is_a(Y, Z).
-
-part_of(X, Z) :- 
-    is_a(X, Y), 
-    part_of(Y, Z).
-
